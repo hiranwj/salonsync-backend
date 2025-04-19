546fdc31a726cd1204c9743e0968420866ba74a0
hiran_xdoto <hiran@xdoto.io>
2025-04-20 01:00:45 +0530

    Initial commit

---
+++ src/main/java/com/hiranwj/salonsync/SalonsyncApplication.java
@@ -0,0 +1,13 @@
+package com.hiranwj.salonsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiranwj/salonsync/SalonsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiranwj.salonsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
